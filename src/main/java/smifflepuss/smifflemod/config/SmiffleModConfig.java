0213c22efa22562fd281cc7a5c49fae472e64bfe
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2024-02-26 16:22:10 -0500

    Bear meat texture changed

--- src/main/java/smifflepuss/smifflemod/config/SmiffleModConfig.java
+++ src/main/java/smifflepuss/smifflemod/config/SmiffleModConfig.java
@@ -15,8 +15,8 @@ public class SmiffleModConfig {
         ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
         ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
 
-        COMMON_BUILDER.comment("Experimental Entity Modifications").push("experimental_entity_modifications");
-        ENABLE_POLAR_BEAR_MEAT = COMMON_BUILDER.comment("Enables polar bears dropping their meat when slain").define("Enable polar bears dropping their meat when slain", true);
+        COMMON_BUILDER.comment("Entity Modifications").push("entity_modifications");
+        ENABLE_POLAR_BEAR_MEAT = COMMON_BUILDER.comment("Enables any polar bear entities that drop their meat when slain").define("Enable polar bears dropping their meat when slain", true);
         COMMON_BUILDER.pop();
 
         CLIENT = CLIENT_BUILDER.build();
