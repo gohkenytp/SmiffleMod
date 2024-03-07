18a651bb3f6cb86760a7f77d841605558e911b21
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2024-03-07 15:44:59 -0500

    Update ServerEvents.java

--- src/main/java/smifflepuss/smifflemod/event/ServerEvents.java
+++ src/main/java/smifflepuss/smifflemod/event/ServerEvents.java
@@ -1,12 +1,15 @@
 package smifflepuss.smifflemod.event;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.PolarBear;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.BasicItemListing;
+import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -25,16 +28,17 @@ public class ServerEvents {
 
     @SubscribeEvent
     public static void onLivingDeathEvent(LivingDeathEvent event) {
-        Level level = event.getEntity().getCommandSenderWorld();
+        Level level = event.getEntity().level();
         BlockPos pos = event.getEntity().blockPosition();
+        Entity entity = event.getSource().getEntity();
         try {
-            if (SmiffleModConfig.ENABLE_POLAR_BEAR_MEAT.get() && event.getEntity().getType() == EntityType.POLAR_BEAR) {
-                int i = event.getEntity().getCommandSenderWorld().getRandom().nextInt(2);
+            if (SmiffleModConfig.ENABLE_POLAR_BEAR_MEAT.get() && entity instanceof PolarBear) {
+                int i = event.getEntity().level().getRandom().nextInt(2);
                 if (i == 1) {
                     ItemEntity itemEntity = new ItemEntity(EntityType.ITEM, level);
                     itemEntity.setPos(pos.getX() + 0.5D, pos.getY() + 0.2D, pos.getZ() + 0.5D);
                     itemEntity.setItem(SmiffleModItems.BEAR_MEAT.get().getDefaultInstance());
-                    event.getEntity().getCommandSenderWorld().addFreshEntity(itemEntity);
+                    event.getEntity().level().addFreshEntity(itemEntity);
                 }
             }
         } catch(Exception e){
