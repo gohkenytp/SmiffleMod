fd242d392abe43e8320803498ff037d9fc1ccbe4
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2024-03-09 14:26:05 -0500

    Update ServerEvents.java

--- src/main/java/smifflepuss/smifflemod/event/ServerEvents.java
+++ src/main/java/smifflepuss/smifflemod/event/ServerEvents.java
@@ -25,24 +25,4 @@ public class ServerEvents {
         List<VillagerTrades.ItemListing> genericTrades = event.getGenericTrades();
         genericTrades.add(new BasicItemListing(1, new ItemStack(SmiffleModItems.BLUEBELL.get()), 12, 1, 0.05F));
     }
-
-    @SubscribeEvent
-    public static void onLivingDeathEvent(LivingDeathEvent event) {
-        Level level = event.getEntity().level();
-        BlockPos pos = event.getEntity().blockPosition();
-        Entity entity = event.getSource().getEntity();
-        try {
-            if (SmiffleModConfig.ENABLE_POLAR_BEAR_MEAT.get() && entity instanceof PolarBear) {
-                int i = event.getEntity().level().getRandom().nextInt(2);
-                if (i == 1) {
-                    ItemEntity itemEntity = new ItemEntity(EntityType.ITEM, level);
-                    itemEntity.setPos(pos.getX() + 0.5D, pos.getY() + 0.2D, pos.getZ() + 0.5D);
-                    itemEntity.setItem(SmiffleModItems.BEAR_MEAT.get().getDefaultInstance());
-                    event.getEntity().level().addFreshEntity(itemEntity);
-                }
-            }
-        } catch(Exception e){
-            SmiffleMod.LOGGER.warn("Tried to add unique behaviors to vanilla mobs when slain and encountered an error");
-        }
-    }
 }
